a3ae35e2a4f340321c179120408485f4bea1e0e6
wibu137 <vokienvantb2020@gmail.com>
2026-03-18 19:20:42 +0700

    fix db

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -11,8 +11,6 @@ export const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
       dbName: 'next-blog',
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
     console.log('Connected to MongoDB');
     initialized = true;
