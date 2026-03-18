b1aeb7c6df5f077c5315d6a0709d7b5f305760a3
wibu137 <vokienvantb2020@gmail.com>
2026-03-18 21:48:05 +0700

    add firebase

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "next-blog-bac5b.firebaseapp.com",
-  projectId: "next-blog-bac5b",
-  storageBucket: "next-blog-bac5b.firebasestorage.app",
-  messagingSenderId: "282459795765",
-  appId: "1:282459795765:web:9392a74d8014b012c2dc7c"
+  authDomain: "blog-kp23.firebaseapp.com",
+  projectId: "blog-kp23",
+  storageBucket: "blog-kp23.firebasestorage.app",
+  messagingSenderId: "914158007428",
+  appId: "1:914158007428:web:4f1e1450b460ef6c0afed1"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
