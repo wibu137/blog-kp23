049391cca44b64bffabc98a3796ca91c0eafb9c7
wibu137 <vokienvantb2020@gmail.com>
2026-03-18 21:20:08 +0700

    fix

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -1,21 +1,53 @@
+// import mongoose from 'mongoose';
+
+// let initialized = false;
+
+// export const connect = async () => {
+//   mongoose.set('strictQuery', true);
+//   if (initialized) {
+//     console.log('Already connected to MongoDB');
+//     return;
+//   }
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI, {
+//       dbName: 'next-blog',
+      
+//     });
+//     console.log('Connected to MongoDB');
+//     initialized = true;
+//   } catch (error) {
+//     console.log('Error connecting to MongoDB:', error);
+//   }
+// };
 import mongoose from 'mongoose';
 
-let initialized = false;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error('Please define MONGODB_URI');
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
 
 export const connect = async () => {
-  mongoose.set('strictQuery', true);
-  if (initialized) {
-    console.log('Already connected to MongoDB');
-    return;
+  if (cached.conn) {
+    return cached.conn;
   }
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
       dbName: 'next-blog',
-      
+      bufferCommands: false, // 🔥 QUAN TRỌNG
     });
-    console.log('Connected to MongoDB');
-    initialized = true;
-  } catch (error) {
-    console.log('Error connecting to MongoDB:', error);
   }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
